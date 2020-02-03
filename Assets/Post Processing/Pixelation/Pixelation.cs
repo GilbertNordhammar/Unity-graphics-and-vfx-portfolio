c9ccbb87099280191d2ff8838b8f9526fe9fc15b
Gilbert Nordhammar <gilbertnordhammar@gmail.com>
2020-02-03 22:51:57 +0100

    Made a pixelation post processing effect

---
+++ Assets/Post Processing/Pixelation/Pixelation.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+using System;
+
+[Serializable, VolumeComponentMenu("Post-processing/Custom/Pixelation")]
+public sealed class Pixelation : CustomPostProcessVolumeComponent, IPostProcessComponent
+{
+    public BoolParameter _enabled = new BoolParameter(true);
+    public Vector2Parameter _resolution = new Vector2Parameter(new Vector2(100, 100));
+
+    Material m_Material;
+
+    public bool IsActive() => m_Material != null && _enabled.value == true;
+
+    // Do not forget to add this post process in the Custom Post Process Orders list (Project Settings > HDRP Default Settings).
+    public override CustomPostProcessInjectionPoint injectionPoint => CustomPostProcessInjectionPoint.AfterPostProcess;
+
+    const string kShaderName = "Hidden/Shader/Pixelation";
+    
+    public override void Setup()
+    {
+        if (Shader.Find(kShaderName) != null)
+            m_Material = new Material(Shader.Find(kShaderName));
+        else
+            Debug.LogError($"Unable to find shader '{kShaderName}'. Post Process Volume Pixelation is unable to load.");
+    }
+    
+    public override void Render(CommandBuffer cmd, HDCamera camera, RTHandle source, RTHandle destination)
+    {
+        if (m_Material == null)
+            return;
+
+        m_Material.SetTexture("_inputTexture", source);
+        m_Material.SetVector("_resolution", _resolution.value);
+        HDUtils.DrawFullScreen(cmd, m_Material, destination);
+    }
+
+    public override void Cleanup()
+    {
+        CoreUtils.Destroy(m_Material);
+    }
+}
